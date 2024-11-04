bae83271a2ee743e7d98e5ccaddcff00014cd256
Syed Azeez <syedazeez337@gmail.com>
2024-11-04 11:44:43 +0530

    new changes

--- main.go
+++ main.go
@@ -1,7 +1,11 @@
 package main
 
-import "moneyconverter/money"
+import (
+	"fmt"
+	"strconv"
+)
 
 func main() {
-	money.ParseDecimal("HelloWorld")
+	value, err := strconv.ParseInt("1010", 2, 64)
+	fmt.Printf("%v - %v\n", value, err)
 }
--- money/decimal.go
+++ money/decimal.go
@@ -1,11 +1,13 @@
 package money
 
 import (
+	"fmt"
+	"strconv"
 	"strings"
 )
 
 type Decimal struct {
-	subunits int64
+	subunits  int64
 	precision byte
 }
 
@@ -17,4 +19,23 @@ const (
 
 func ParseDecimal(value string) (Decimal, error) {
 	intPart, fractPart, _ := strings.Cut(value, ".")
+
+	const maxDecimal = 12
+	if len(intPart) > maxDecimal {
+		return Decimal{}, ErrTooLarge
+	}
+
+	combinedValue := intPart + fractPart
+
+	subunits, err := strconv.ParseInt(combinedValue, 10, 64)
+	if err != nil {
+		return Decimal{}, fmt.Errorf("%w: %s", ErrInvalidDecimal, err.Error())
+	}
+
+	precision := byte(len(fractPart))
+
+	return Decimal{
+		subunits:  subunits,
+		precision: precision,
+	}, nil
 }
